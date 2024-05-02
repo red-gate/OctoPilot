bbd0e8eaffbf2b4b80be09986a60297d7936ab10
WIN2016\redgate <redgate@redgate.com>
2024-05-02 03:15:17 +0100

    Made DB Changes

---
+++ schema-model/Tables/dbo.WidgetBookSupply.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[WidgetBookSupply]
+(
+[BookSupplyID] [nchar] (10) NOT NULL
+)
+GO
+ALTER TABLE [dbo].[WidgetBookSupply] ADD CONSTRAINT [PK_WidgetSupply] PRIMARY KEY CLUSTERED ([BookSupplyID])
+GO
--- schema-model/Tables/dbo.Widgets.sql
+++ schema-model/Tables/dbo.Widgets.sql
@@ -3,7 +3,8 @@ CREATE TABLE [dbo].[Widgets]
 [RecordID] [int] NOT NULL IDENTITY(1, 1),
 [Description] [varchar] (50) NULL,
 [Twitter] [varchar] (50) NULL,
-[RedgateID] [nchar] (10) NULL
+[RedgateID] [nchar] (10) NULL,
+[Cen_Reg_Num] [int] NULL
 )
 GO
 ALTER TABLE [dbo].[Widgets] ADD CONSTRAINT [PK_Widgets] PRIMARY KEY NONCLUSTERED ([RecordID])
