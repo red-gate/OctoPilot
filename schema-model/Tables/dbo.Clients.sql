29403907014e745d1e182358879c16f32270a2c8
WIN2016\redgate <redgate@redgate.com>
2024-05-02 02:08:05 +0100

    Changes

--- schema-model/Tables/dbo.Clients.sql
+++ schema-model/Tables/dbo.Clients.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[Clients]
 (
 [ProductName] [nchar] (10) NULL,
 [Cost] [nchar] (10) NULL,
-[Client] [nchar] (10) NULL
+[Client] [nchar] (10) NULL,
+[Client_Name] [nchar] (10) NULL
 )
 GO
