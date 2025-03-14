ade73e678e9108198d2d373fc884b97c16124f9c
WIN2016\redgate <redgate@redgate.com>
2025-03-14 11:06:02 +0000

    Huxley SB Test

---
+++ migrations/V011_20250314110445__StandardBankTest.sql
@@ -0,0 +1,14 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[IDAddressBook]'
+GO
+CREATE TABLE [dbo].[IDAddressBook]
+(
+[FirstName] [nchar] (10) NULL,
+[LastName] [nchar] (10) NULL,
+[ID] [nchar] (10) NULL
+)
+GO
+
---
+++ schema-model/Tables/dbo.IDAddressBook.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[IDAddressBook]
+(
+[FirstName] [nchar] (10) NULL,
+[LastName] [nchar] (10) NULL,
+[ID] [nchar] (10) NULL
+)
+GO
