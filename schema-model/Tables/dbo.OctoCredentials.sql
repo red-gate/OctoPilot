25dbda0f487d18841813949b39d20cd9e6fde6bd
WIN2016\redgate <redgate@redgate.com>
2025-05-30 16:12:08 +0100

    hk

--- schema-model/Tables/dbo.OctoCredentials.sql
+++ schema-model/Tables/dbo.OctoCredentials.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[OctoCredentials]
 [OctopusID] [nchar] (10) NULL,
 [OctopusURL] [nchar] (10) NULL,
 [OctopusAPI] [nchar] (10) NULL,
-[OctoTentacle] [nchar] (10) NULL
+[OctoTentacle] [nchar] (10) NULL,
+[Test] [nchar] (10) NULL
 )
 GO
